940660b651c4d315d9c520be5b9d7383f42cd89b
Nomaan <nomaan.alkurn@gmail.com>
2018-05-16 13:00:29 +0530

    array spread operator

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
-function sumAll(...arr) {
-    return arr.reduce((prev, next) => prev + next);
-}
+const toppings = ['bacon', 'chilli'];
 
-console.log(sumAll(1,2,3,4,5,6,7,8,9,10));
+const newToppings = ['pepporoni'];
+
+const allToppings = [...toppings, ...newToppings];
+
+console.log(allToppings)
