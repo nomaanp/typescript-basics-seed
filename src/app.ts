978a84b3c8c7f81caed3bf66957efb91874a2b4c
Nomaan <nomaan.alkurn@gmail.com>
2018-05-16 12:03:50 +0530

    arrow functions

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,18 @@
-console.log('Hey TypeScript!');
+const pizzas = [
+    { name: 'Pepporoni', toppings: ['pepporoni']}
+];
+
+var mapPizzas = pizzas.map(pizza =>  pizza.name.toUpperCase())
+
+// var mapPizzas = pizzas.map(pizza => {
+//     return pizza.name.toUpperCase();
+// })
+
+console.log(mapPizzas)
+
+const pizza = {
+    name: 'Blazing Inferno',
+    getName: () => pizza.name
+}
+
+console.log(pizza.getName())
