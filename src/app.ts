d3ddbdd4d892c63ecc2c25eac927aa2d725c85a0
Nomaan <nomaan.alkurn@gmail.com>
2018-05-16 12:40:35 +0530

    rest parameters

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,5 @@
-const pizza = {
-    name: 'Pepporoni',
-    price: 20,
-    getName() {
-        return this.name;
-    }
+function sumAll(...arr) {
+    return arr.reduce((prev, next) => prev + next);
 }
 
-console.log(pizza.getName())
-
-const toppings = ['pepporoni'];
-
-// const order = { pizza, toppings };
-
-// console.log(order)
-
-function createOrder(pizza, toppings) {
-    return { pizza, toppings };
-}
-
-console.log(createOrder(pizza, toppings));
+console.log(sumAll(1,2,3,4,5,6,7,8,9,10));
