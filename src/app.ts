5c58a436333cde55965b885fcc02c7cdd17747dd
Nomaan <nomaan.alkurn@gmail.com>
2018-05-30 05:12:50 +0530

    classes introductions

--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,19 @@
-interface Sizes {
-    sizes: string[];
-}
-
-interface Pizza extends Sizes {
+class Pizza {
     name: string;
-    toppings?: number;
-    getAvailableSizes(): string[];
-    [key: number]: string;  
-    dictionary: {
-        [key: string]: any;
-    }  
-};
+    toppings: string[] = [];
 
-interface Pizzas {
-    data: Pizza[]
+    constructor(name: string) {
+        this.name = name;
+    }
+
+    addTopping(topping: string) {
+        this.toppings.push(topping);
+    }
 }
 
-let pizza: Pizza;
 
-function createPizza(name: string, sizes: string[]): Pizza {
-    return {
-        name,
-        sizes,
-        getAvailableSizes() {
-            return this.sizes;
-        },
-    };
-}
+const pizza = new Pizza('Pepperoni');
 
-pizza = createPizza('Pepperoni', ['small', 'medium']);
-pizza[1] = 'xyz';
-pizza.toppings = 1;
+pizza.addTopping('pepperoni');
+
+console.log(pizza);
