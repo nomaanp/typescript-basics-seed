74b7b7b2a90ec0a1e34cf0dbd3ec45ebb1d54209
Nomaan <nomaan.alkurn@gmail.com>
2018-05-30 04:56:49 +0530

    optional arguments in interface

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ interface Sizes {
 
 interface Pizza extends Sizes {
     name: string;
+    toppings?: number;
     getAvailableSizes(): string[];    
 };
 
@@ -24,3 +25,5 @@ function createPizza(name: string, sizes: string[]): Pizza {
 }
 
 pizza = createPizza('Pepperoni', ['small', 'medium']);
+
+pizza.toppings = 1;
