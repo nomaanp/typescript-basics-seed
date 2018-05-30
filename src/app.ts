a8ae763067328f36f0a7ce85c1dd33ef4958beac
Nomaan <nomaan.alkurn@gmail.com>
2018-05-30 20:20:53 +0530

    class inheritance

--- src/app.ts
+++ src/app.ts
@@ -10,27 +10,30 @@ class Sizes {
     }
 }
 
-const sizes = new Sizes(['small', 'medium']);
+/*const sizes = new Sizes(['small', 'medium']);
 
 // invoke setter
 sizes.availableSizes = ['medium', 'large'];
 
 // invoke getter
-console.log(sizes.availableSizes);
+console.log(sizes.availableSizes);*/
 
-class Pizza {
+class Pizza extends Sizes {
     public toppings: string[] = [];
 
-    constructor(readonly name: string) {}
+    constructor(readonly name: string, public sizes: string[]) {
+        super(sizes);
+    }
 
     public addTopping(topping: string) {
         this.toppings.push(topping);
     }
 }
 
-
-const pizza = new Pizza('Pepperoni');
+const pizza = new Pizza('Pepperoni', ['small', 'medium']);
 
 pizza.addTopping('pepperoni');
 
 console.log(pizza.name);
+
+console.log(pizza.availableSizes);
